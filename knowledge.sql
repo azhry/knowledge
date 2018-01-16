bb0576ac8df3c337330b7b3b1213570641f19490
azhry <arliansyah_azhary@yahoo.com>
2018-01-16 12:44:54 +0700

    nak bimbingan

--- knowledge.sql
+++ knowledge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2018 at 07:26 PM
+-- Generation Time: Jan 16, 2018 at 06:43 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
